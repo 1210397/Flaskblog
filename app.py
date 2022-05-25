2a5435ecc46aad4123bb624917d693393d729e96
1210397 <cbmt1210397@gn.iwasaki.ac.jp>
2022-05-25 13:43:43 +0900

    Create app.py

--- app.py
+++ app.py
@@ -1,24 +1,32 @@
-from flask import Flask
-from flask import render_template
-from flask import request
-from flask import redirect
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import pytz
+from flask_login import UserMixin,LoginManager
+import os
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-db = SQLAlchemy(app)
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///blog.db'
+app.config['SECRET_KEY']=os.urandom(24)
+db=SQLAlchemy(app)
+login_manager=LoginManager()
+login_manager.init_app(app)
 
 class Post(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    title=db.Column(db.String(50),nullable=False)
-    body=db.Column(db.String(300),nullable=False)
-    created_at=db.Column(db.DateTime,nullable=False,default=datetime.now(pytz.timezone("Asia/Tokyo")))
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(50),nullable=False)
+    body = db.Column(db.String(300),nullable=False)
+    created_at = db.Column(db.DateTime,nullable=False,default=datetime.now(pytz.timezone("Asia/Tokyo")))
 
-@app.route("/")
+class User(UserMixin,db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(20),unique=True)
+    password = db.Column(db.String(20))
+
+@app.route("/",methods=["GET"])
 def index():
-    return render_template("index.html")
+    posts = Post.query.all() 
+    return render_template("index.html",posts=posts)
 
 @app.route("/article1")
 def article1():
@@ -41,6 +49,24 @@ def create():
         return redirect("/")
     else:
         return render_template("create.html")
+
+@app.route("/<int:id>/update",methods=["GET","POST"]) 
+def update(id):
+    post=Post.query.get(id)
+    if request.method == "GET":
+        return render_template("update.html",post=post)
+    else:
+        post.title = request.form.get("title")
+        post.body = request.form.get("body")
+        db.session.commit()
+        return redirect("/")
+
+@app.route("/<int:id>/delete",methods=["GET"])
+def delete(id):
+    post = Post.query.get(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run(debug=True)
